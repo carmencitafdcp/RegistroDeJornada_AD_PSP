666a253d9d7915c32b19afc816dd376140321c62
Carmen Fernández de Córdoba <carmenfdcpwork@gmail.com>
2025-11-26 09:07:34 +0100

    creación del proyecto de registro de jornada

---
+++ registro_de_jornada/src/main/java/com/salesianostriana/dam/registro_de_jornada/RegistroDeJornadaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.registro_de_jornada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroDeJornadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroDeJornadaApplication.class, args);
+	}
+
+}
---
+++ registro_de_jornada/src/test/java/com/salesianostriana/dam/registro_de_jornada/RegistroDeJornadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.registro_de_jornada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroDeJornadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
